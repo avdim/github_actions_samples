4f9dcf0e3b3b9417f4b0984ed80ef26146d2b44f
Dmitriy <avdim@mail.ru>
2020-01-17 18:52:45 +0300

    Update start_emulator.sh

--- start_emulator.sh
+++ start_emulator.sh
@@ -19,7 +19,7 @@ AVD_MANAGER=$ANDROID_HOME/tools/bin/avdmanager
 mkdir --mode 777 -p "$ANDROID_HOME"
 cd "$ANDROID_HOME"
 curl -o sdk.zip $SDK_URL
-unzip -qq sdk.zip > /dev/null 2>&1 &
+unzip -qq sdk.zip > /dev/null 2>&1
 rm sdk.zip
 yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
 
