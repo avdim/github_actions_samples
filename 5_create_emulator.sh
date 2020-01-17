480b2f54c77c6cdcf4924458f87a81223007478b
Dmitriy <avdim@mail.ru>
2020-01-17 23:17:33 +0300

    Update 5_create_emulator.sh

--- 5_create_emulator.sh
+++ 5_create_emulator.sh
@@ -6,5 +6,5 @@ set -o xtrace
 echo no | $AVD_MANAGER create avd --force --name testAvd --abi google_apis/x86_64 --package 'system-images;android-${ANDROID_VERSION};google_apis;x86_64' > /dev/null 2>&1 &
 $EMULATOR -avd testAvd -no-window -no-boot-anim -no-audio -verbose > /dev/null 2>&1 &
 
-sleep 200
+sleep 60
 $ADB devices
