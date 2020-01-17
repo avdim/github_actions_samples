4f00ace8531840b82deac4539aab7743c243b9a8
Dmitriy <avdim@mail.ru>
2020-01-17 17:47:36 +0300

    Update start_emulator.sh

--- start_emulator.sh
+++ start_emulator.sh
@@ -26,7 +26,7 @@ $EMULATOR -avd testAvd -no-window -no-boot-anim -no-audio -verbose > /dev/null 2
 #$EMULATOR -avd testAvd -no-audio -no-window > /dev/null 2>&1 &
 $ADB devices
 #./wait_for_emulator.sh
-sleep 60
+sleep 20
 
 #$ADB -e shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > /tmp/screen.png
 
