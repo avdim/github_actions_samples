d2613b00dabc9b4a36fec18929dcd1fa8ed8e259
Dmitriy <avdim@mail.ru>
2020-01-17 15:49:09 +0300

    Update start_emulator.sh

--- start_emulator.sh
+++ start_emulator.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-kill_all_emulators() {
-    adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
-}
-
 set -o xtrace
 
 #Задать ANDROID_HOME если ещё не задано в системе
@@ -14,6 +10,10 @@ SDK_MANAGER=$ANDROID_HOME/tools/bin/sdkmanager
 EMULATOR=$ANDROID_HOME/emulator/emulator
 AVD_MANAGER=$ANDROID_HOME/tools/bin/avdmanager
 
+kill_all_emulators() {
+    $ADB devices | grep emulator | cut -f1 | while read line; do $ADB -s $line emu kill; done
+}
+
 $ADB kill-server
 $SDK_MANAGER "emulator"
 yes | $SDK_MANAGER "system-images;android-29;google_apis;x86_64" #default
@@ -22,7 +22,7 @@ echo no | $AVD_MANAGER create avd --force --name testAvd --abi google_apis/x86_6
 
 $EMULATOR -list-avds
 kill_all_emulators
-$EMULATOR -avd testAvd -no-audio > /dev/null 2>&1 &
+$EMULATOR -avd testAvd -no-window -no-boot-anim -no-audio -verbose > /dev/null 2>&1 &
 #$EMULATOR -avd testAvd -no-audio -no-window > /dev/null 2>&1 &
 $ADB devices
 sleep 20
