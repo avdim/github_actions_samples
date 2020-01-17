9eef6e2037d572989de5bc91d13029a45033e1ce
Dmitriy <avdim@mail.ru>
2020-01-17 17:39:29 +0300

    Update start_emulator_in_docker.sh

--- start_emulator_in_docker.sh
+++ start_emulator_in_docker.sh
@@ -10,7 +10,7 @@ SDK_MANAGER=$ANDROID_HOME/tools/bin/sdkmanager
 EMULATOR=$ANDROID_HOME/emulator/emulator
 AVD_MANAGER=$ANDROID_HOME/tools/bin/avdmanager
 
-docker run -it --rm -p 6080:6080 -p 5565:5555 -p 5564:5554 --name avd29 --device /dev/kvm avdim/avd29
+docker run --rm -p 6080:6080 -p 5565:5555 -p 5564:5554 --name avd29 --device /dev/kvm avdim/avd29
 $ADB connect localhost:5565
 $ADB devices
 $ADB -e shell screencap /sdcard/screen.png
